6dca6d08648edb7fcbc49a90205e33f892b00f62
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-10-03 23:16:18 -0300

    header home component

---
+++ src/App/components/Header/componentsStyled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Title = styled.h2`
+  color: #FFFFFF;
+  margin-left: 26px;
+`;
+
+export const Container = styled.div`
+    background-color:#343C58;
+    width: 100%;
+    height: 56px;
+    display: flex;
+    align-items: center;
+`
---
+++ src/App/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Title, Container} from './componentsStyled'
+
+const Header = () => {
+    return (
+        <Container>
+            <Title>Teste Dev Frontend</Title>
+        </Container>
+    )
+}
+
+export default Header
