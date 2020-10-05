30f984637ec48a8dd452a7c34796f307c25856a9
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-10-05 09:23:23 -0300

    add NotFound page

--- src/App/Router.js
+++ src/App/Router.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch } from "wouter";
 import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 const Router = () => {
     return (
@@ -9,6 +10,9 @@ const Router = () => {
                 {params => (
                     <Home category={params.category} />
                 )}
+            </Route>
+            <Route path="/:rest*">
+                {(params) => <NotFound />}
             </Route>
         </Switch>
     )
